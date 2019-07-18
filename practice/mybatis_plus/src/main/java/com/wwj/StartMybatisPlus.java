cae70209974ccda46502f7d64958b34626527b91
Nancy <wangwei_jun@163.com>
2019-07-18 21:46:39 +0800

    线程原理(域)

--- practice/mybatis_plus/src/main/java/com/wwj/StartMybatisPlus.java
+++ practice/mybatis_plus/src/main/java/com/wwj/StartMybatisPlus.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("com.tj.practice.*.mapper")
+@MapperScan("com.wwj.*.mapper")
 public class StartMybatisPlus {
 
 	public static void main(String[] args) {
