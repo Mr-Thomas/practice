01d1d4d1b5904274ab1ae61cf7bc4935c45dbf53
Thomas <wangwei_jun@163.com>
2020-01-19 12:28:24 +0800

    kafka测试类

---
+++ practice/src/test/java/com/tj/practice/KafkaTest.java
@@ -0,0 +1,23 @@
+package com.tj.practice;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @Author: Mr.Thomas
+ * @Date: 2020/1/18 19:52
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Slf4j
+public class KafkaTest {
+//    @Autowired
+//    private MessageProducer messageProducer;
+//
+//    @Test
+//    public void producerTest(){
+//        messageProducer.sendMessage("alarm_push_warn","Hello");
+//    }
+}
